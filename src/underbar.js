a3de7681c8bbd402f4c16fb2c754240539e1aae0
Eric <Eric@Erics-MacBook-Air.local>
2015-06-25 15:30:45 -0500

    Filled in _.last function body

--- src/underbar.js
+++ src/underbar.js
@@ -38,6 +38,7 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    return n === undefined ? array.pop(): (n === 0 ? [] : array.slice(-n) );
   };
 
   // Call iterator(value, key, collection) for each element of collection.
